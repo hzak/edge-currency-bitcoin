f98624e400f8e64fec40102192b7c13be576cb66
Hakob Kanoyan <hakob91@gmail.com>
2020-07-27 23:42:08 +0400

    test

--- src/info/zcoins.js
+++ src/info/zcoins.js
@@ -46,16 +46,16 @@ const bcoinInfo: BcoinCurrencyInfo = {
   magic: 0xd9b4bef9,
   formats: ['bip44', 'bip32'],
   keyPrefix: {
-    privkey: 0xd2,
-    xpubkey: 0x0488b21e,
-    xprivkey: 0x0488ade4,
+    privkey: 0xb9,
+    xpubkey: 0x043587cf,
+    xprivkey: 0x04358394,
     xpubkey58: 'xpub',
     xprivkey58: 'xprv',
     coinType: 136
   },
   addressPrefix: {
-    pubkeyhash: 0x52,
-    scripthash: 0x7
+    pubkeyhash: 0x41,
+    scripthash: 0xb2
   }
 }
 
