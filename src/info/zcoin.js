d44374f85c78178fae34e50c721eae98883cfbdd
hzak <hayk.zakaryan@picsart.com>
2020-08-04 00:37:29 +0400

    testnet

--- src/info/zcoin.js
+++ src/info/zcoin.js
@@ -11,16 +11,16 @@ const bcoinInfo: BcoinCurrencyInfo = {
   magic: 0xd9b4bef9,
   formats: ['bip44', 'bip32'],
   keyPrefix: {
-    privkey: 0xd2,
-    xpubkey: 0x0488b21e,
-    xprivkey: 0x0488ade4,
+    privkey: 0xb9,
+    xpubkey: 0x043587cf,
+    xprivkey: 0x04358394,
     xpubkey58: 'xpub',
     xprivkey58: 'xprv',
     coinType: 136
   },
   addressPrefix: {
-    pubkeyhash: 0x52,
-    scripthash: 0x7
+    pubkeyhash: 0x41,
+    scripthash: 0xb2
   }
 }
 
@@ -62,12 +62,14 @@ const currencyInfo: EdgeCurrencyInfo = {
   defaultSettings: {
     customFeeSettings: ['satPerByte'],
     electrumServers: [
-      'electrum://51.15.82.184:50001',
-      'electrum://45.63.92.224:50001',
-      'electrum://47.75.76.176:50001',
-      'electrums://51.15.82.184:50002',
-      'electrums://45.63.92.224:50002',
-      'electrums://47.75.76.176:50002'
+      //'electrum://51.15.82.184:50001',
+      //'electrum://45.63.92.224:50001',
+      //'electrum://47.75.76.176:50001',
+      //'electrums://51.15.82.184:50002',
+      //'electrums://45.63.92.224:50002',
+      //'electrums://47.75.76.176:50002'
+      'electrum://95.179.164.13:51001',
+      'electrum://95.179.164.13:51002'
     ],
     disableFetchingServers: false
   },
